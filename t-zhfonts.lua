2d6a5e05440c7a6fc9a3ae0773451f593df7d2d8
garfileo <lyr.m2@live.cn>
2023-02-24 06:40:39 +0800

    wip

--- t-zhfonts.lua
+++ t-zhfonts.lua
@@ -1,5 +1,6 @@
 moduledata = moduledata or {}
 moduledata.zhfonts = moduledata.zhfonts or {}
+
 local zhfonts = moduledata.zhfonts
 local zhspuncs = require "t-zhspuncs"
 
@@ -44,7 +45,7 @@ local function init_fonts_table()
     return f
 end
 
-fonts.protrusions.vectors['myvector'] = {  
+fonts.protrusions.vectors["myvector"] = {  
    [0xFF0c] = { 0, 0.60 },  -- ，
    [0x3002] = { 0, 0.60 },  -- 。
    [0x2018] = { 0.60, 0 },  -- ‘
@@ -59,8 +60,9 @@ fonts.protrusions.vectors['myvector'] = {
    [0x3001] = { 0, 0.50 },  -- 、
    [0xFF0E] = { 0, 0.50 },  -- ．
 }
-fonts.protrusions.classes['myvector'] = {
-   vector = 'myvector', factor = 1
+fonts.protrusions.classes["myvector"] = {
+    vector = "myvector",
+    factor = 1
 }
 
 local text_fonts = init_fonts_table()
@@ -78,15 +80,18 @@ function zhfonts.gen_typescript()
     for _, a in pairs(style) do
         for _, b in pairs(type) do
             local fontname = a .. b
-            rep[k] = {"zh" .. fontname .. "!", "name:" .. text_fonts.chinese[a][b].name}
-            rep[k + 1] = {"zh" .. fontname .. "@rscale!", text_fonts.chinese[a][b].rscale}
-            rep[k + 2] = {"latin" .. fontname .. "!", "name:" .. text_fonts.latin[a][b]}
+            rep[k] = {"zh" .. fontname .. "!",
+                      "name:" .. text_fonts.chinese[a][b].name}
+            rep[k + 1] = {"zh" .. fontname .. "@rscale!",
+                          text_fonts.chinese[a][b].rscale}
+            rep[k + 2] = {"latin" .. fontname .. "!",
+                          "name:" .. text_fonts.latin[a][b]}
             k = k + 3
         end
     end
     -- substitute math fonts.
     rep[k] = {"mathtypescriptname!", math_typescript}
-    rep[k + 1] = {"zhfeatures!", fontfeatures}
+    rep[k + 1] = {"features!", fontfeatures}
     local real_typescript = lpeg.replacer(rep):match(typescript)
     context(real_typescript)
     template:close()
@@ -112,19 +117,27 @@ end
 local function setup_math_typescript(name)
     math_typescript = string_strip(name)
 end
-
 local function setup_fontfeatures (s)
     fontfeatures = fontfeatures .. "," .. s
 end
 function zhfonts.setup(metainfo, fontinfo)
     local m = string_split_and_strip(metainfo, ",")
     local f = string_split_and_strip(fontinfo, ",")
-    if #m == 1 and m[1] == "features" then setup_fontfeatures(fontinfo) end
-    if #m == 1 and text_fonts.chinese[m[1]] then setup_chinesefonts(m[1], f)  end
-    if #m == 1 and m[1] == "math" then setup_math_typescript(f[1]) end
+    if #m == 1 and m[1] == "features" then
+        setup_fontfeatures(fontinfo)
+    end
+    if #m == 1 and text_fonts.chinese[m[1]]then
+        setup_chinesefonts(m[1], f)
+    end
+    if #m == 1 and m[1] == "math" then
+        setup_math_typescript(f[1]) end
     if #m == 2 then
-	if m[1] == "latin" and text_fonts.latin[m[2]] then setup_latinfonts(m[2], f) end
-	if m[2] == "latin" and text_fonts.latin[m[1]] then setup_latinfonts(m[1], f) end	
+	if m[1] == "latin" and text_fonts.latin[m[2]] then
+            setup_latinfonts(m[2], f)
+        end
+	if m[2] == "latin" and text_fonts.latin[m[1]] then
+            setup_latinfonts(m[1], f)
+        end	
     end
 end
 
@@ -136,8 +149,8 @@ function zhfonts.main(param)
         zhfonts.gen_typescript()
         if arg_list[1] ~= "hack" and arg_list[2] ~= "hack" then
             context("\\usetypescript[zhfonts]")
-            context("\\setupbodyfont[zhfonts, " .. param .. "]") 
+            context("\\setupbodyfont[zhfonts, " .. param .. "]")
+            context("\\setupalign[hanging, hz]")
         end
-        context("\\setupalign[hanging, hz]")
     end
 end
